faf384a1b9c2b8bc7dc7b6f260b12a1fa94dd092
Dayne Dewar <daynedewar@yahoo.com>
2020-12-28 03:01:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,6 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   new_hash = {
     :name => 'Grace Hopper'
   }
@@ -18,4 +17,7 @@ end
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  new_hash = {
+    :id => number
+  }
 end
